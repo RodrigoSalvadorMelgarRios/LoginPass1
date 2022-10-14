26230fe008bd5fa5220efb5279276ea22581b952
Alumno <Alumno@DESKTOP-VF44KE1>
2022-10-14 18:15:54 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.vb
@@ -0,0 +1,103 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.Textemail = New System.Windows.Forms.TextBox()
+        Me.TextPass = New System.Windows.Forms.TextBox()
+        Me.LoginButton = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(358, 187)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(12, 219)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(108, 149)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'Textemail
+        '
+        Me.Textemail.Location = New System.Drawing.Point(35, 268)
+        Me.Textemail.Name = "Textemail"
+        Me.Textemail.Size = New System.Drawing.Size(312, 23)
+        Me.Textemail.TabIndex = 2
+        '
+        'TextPass
+        '
+        Me.TextPass.Location = New System.Drawing.Point(35, 363)
+        Me.TextPass.Name = "TextPass"
+        Me.TextPass.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.TextPass.Size = New System.Drawing.Size(312, 23)
+        Me.TextPass.TabIndex = 3
+        '
+        'LoginButton
+        '
+        Me.LoginButton.Location = New System.Drawing.Point(106, 416)
+        Me.LoginButton.Name = "LoginButton"
+        Me.LoginButton.Size = New System.Drawing.Size(153, 23)
+        Me.LoginButton.TabIndex = 4
+        Me.LoginButton.Text = "Login"
+        Me.LoginButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(385, 465)
+        Me.Controls.Add(Me.LoginButton)
+        Me.Controls.Add(Me.TextPass)
+        Me.Controls.Add(Me.Textemail)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Textemail As TextBox
+    Friend WithEvents TextPass As TextBox
+    Friend WithEvents LoginButton As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+    Dim user As String
+    Dim Pass As String
+
+    Private Sub Textemail_TextChanged(sender As Object, e As EventArgs) Handles Textemail.TextChanged
+        user = Textemail.Text
+
+    End Sub
+
+    Private Sub TextPass_TextChanged(sender As Object, e As EventArgs) Handles TextPass.TextChanged
+        Pass = TextPass.Text
+    End Sub
+
+    Private Sub LoginButton_Click(sender As Object, e As EventArgs) Handles LoginButton.Click
+        If InStr(1, user, "rodrigomelgar@hotmail.com") = 0 Then
+            MessageBox.Show("Por favor ingrese un correo válido", "ERROR")
+        ElseIf InStr(1, Pass, "Senati22") = 0 Then
+            MessageBox.Show("Contraseña invalida")
+        ElseIf InStr(1, Pass, "Senati22") = 1 Then
+            MessageBox.Show("Bienvenido Honorable Usuario")
+
+        End If
+    End Sub
+End Class
